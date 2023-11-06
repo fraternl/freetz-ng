dda715a47a21d98385844f03ca1bbc91ca4f2fe9
fraternl <frater@hetemail.com>
2023-11-06 23:45:55 +0100

    Update 00-password.sh
    Extend warning, so it also warns for an alternate default password (the one printed on the back of modern Fritzboxes)

--- make/pkgs/mod/files/root/usr/mww/cgi-bin/status.d/00-password.sh
+++ make/pkgs/mod/files/root/usr/mww/cgi-bin/status.d/00-password.sh
@@ -1,6 +1,10 @@
 default_password_set() {
-	[ "$MOD_HTTPD_PASSWD" == '$1$$zO6d3zi9DefdWLMB.OHaO.' ]
+        [ "$MOD_HTTPD_PASSWD" == '$1$$zO6d3zi9DefdWLMB.OHaO.' ] || [ "$MOD_HTTPD_PASSWD" == "$(md5pw "$BACKPASS")"  ]
 }
+
+md5pw() { /bin/busybox cryptpw -m md5 -S "" "$1"; }
+
+BACKPASS=`grep '^webgui_pass'  /proc/sys/urlader/environment | awk '{print $2}' | egrep -o '[a-z]+[0-9]+'`
 
 if default_password_set; then
 	print_warning "$(lang \
